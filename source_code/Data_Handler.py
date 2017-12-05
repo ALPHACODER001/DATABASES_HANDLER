2faa3897c989d38eaf1aa2af5bd7c1f8c910e0e1
ALPHACODER001 <zaidzaghloul96@gmail.com>
2017-12-05 21:44:47 +0200

    Update Data_Handler.py

--- source_code/Data_Handler.py
+++ source_code/Data_Handler.py
@@ -1,5 +1,6 @@
 
 
+
 import pandas as pd
 import numpy as np
 import os
@@ -7,10 +8,12 @@ import json
 import sys
 
 vague_for_computer=["false","False","F","f"]
-if sys.argv[1] in vague_for_computer:
-    write_on_file=not(bool(sys.argv[1]))
-else :
-    write_on_file=bool(sys.argv[1])
+
+if len(sys.argv)>1:
+    if sys.argv[1] in vague_for_computer:
+        write_on_file=not(bool(sys.argv[1]))
+    else :
+        write_on_file=bool(sys.argv[1])
 
 
 def write_json(json_structure):
@@ -19,7 +22,7 @@ def write_json(json_structure):
         f.write(str(json_structure))
         f.close()
 
-def get_table_rows(file_name):
+def get_table_rows(file_name,type_of_data):
     """the function return list of dicts , by extracting the tables"""
 
     path_to_data=os.getcwd().split("/")
@@ -27,6 +30,10 @@ def get_table_rows(file_name):
     path_to_data="/".join(path_to_data)
 
     df = pd.read_csv(path_to_data+file_name+'.csv')
+
+    if "table" in type_of_data:
+        return df
+
     Blocks=dict()
     List_of_Rows=list()
 
@@ -38,6 +45,10 @@ def get_table_rows(file_name):
 
     return List_of_Rows
 
-file_name=sys.argv[2] # provide name without externsion of the file like trucks
-json_structure=json.dumps(get_table_rows(file_name))
-write_json(json_structure)
+
+
+
+if len(sys.argv)>1:
+    file_name=sys.argv[2] # provide name without externsion of the file like trucks
+    json_structure=json.dumps(get_table_rows(file_name))
+    write_json(json_structure)
