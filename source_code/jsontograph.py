919ac0bfbe4e5504c533a8fe46a967dbfcdb6d00
DarkenedBlade <20140812omarawajan@gmail.com>
2017-12-07 20:19:53 +0200

    Update jsontograph.py

--- source_code/jsontograph.py
+++ source_code/jsontograph.py
@@ -2,7 +2,7 @@ import json
 from py2neo import Graph,Relationship,Node
 
 graph = Graph()
-remote_graph = Graph("localhost/7474", "neo4j", "80320171688")
+remote_graph = Graph("localhost/7474", "neo4j", "yourpassword")
 alice = Node("person", name = "alice")
 bob = Node("person", name = "Bob")
 alice_knows_Bob = Relationship(alice, "knows", bob)
