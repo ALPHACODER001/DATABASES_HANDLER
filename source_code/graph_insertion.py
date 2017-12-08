f710d87df272e11f64e97f00567427522176c51d
ALPHACODER001 <zaidzaghloul96@gmail.com>
2017-12-08 13:11:35 +0200

    Update graph_insertion.py

--- source_code/graph_insertion.py
+++ source_code/graph_insertion.py
@@ -3,16 +3,15 @@
 import sys
 #choosing the directory of packages , based in your python version change the number ,
 sys.path.append('/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages')
-
 import json
 from py2neo import Graph,Relationship,Node
 
-graph = Graph()
 your_Database_port_that_running="7474" # in my case , thats the port , be assure that the port is running from neo4js
 Password="neo4j"
 user_name="neo4j"
 local_host="127.0.0.1/"
 
+graph = Graph()
 remote_graph = Graph(local_host+your_Database_port_that_running, user_name, Password)
 alice = Node("person", name = "alice")
 bob = Node("person", name = "Bob")
