5ff2ffad4c5a000945c74aa203274165fc3b9712
ALPHACODER001 <zaidzaghloul96@gmail.com>
2017-12-08 13:13:57 +0200

    Update graph_insertion.py

--- source_code/graph_insertion.py
+++ source_code/graph_insertion.py
@@ -1,5 +1,4 @@
 
-
 import sys
 #choosing the directory of packages , based in your python version change the number ,
 sys.path.append('/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages')
@@ -11,9 +10,18 @@ Password="neo4j"
 user_name="neo4j"
 local_host="127.0.0.1/"
 
+
+
 graph = Graph()
 remote_graph = Graph(local_host+your_Database_port_that_running, user_name, Password)
+
+## use the Data_handler module to fill , nodes , it's already return dictonary
+## loop through keys , for each key extract it's value and fill it in the ndoe 
+
 alice = Node("person", name = "alice")
 bob = Node("person", name = "Bob")
 alice_knows_Bob = Relationship(alice, "knows", bob)
+
+
+
 graph.create(alice_knows_Bob)
