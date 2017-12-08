844950808e68964467a12c5b4f447c2ba28d5a69
ALPHACODER001 <zaidzaghloul96@gmail.com>
2017-12-08 13:09:46 +0200

    Update jsontograph.py

--- source_code/jsontograph.py
+++ source_code/jsontograph.py
@@ -1,8 +1,18 @@
+
+
+import sys
+#choosing the directory of packages , based in your python version change the number ,
+sys.path.append('/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages')
+
 import json
 from py2neo import Graph,Relationship,Node
 
 graph = Graph()
-remote_graph = Graph("localhost/7474", "neo4j", "yourpassword")
+your_Database_port_that_running="7474" # in my case , thats the port , be assure that the port is running from neo4js
+Password="neo4j"
+user_name="neo4j"
+
+remote_graph = Graph("127.0.0.1/"+your_Database_port_that_running, user_name, Password)
 alice = Node("person", name = "alice")
 bob = Node("person", name = "Bob")
 alice_knows_Bob = Relationship(alice, "knows", bob)
